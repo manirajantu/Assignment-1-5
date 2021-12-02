16bf091717540dffd9aecbb0da8fb964bef2ddd3
manirajantu <94602111+manirajantu@users.noreply.github.com>
2021-12-02 11:47:54 +0800

    assignment-1-5

---
+++ rps.js
@@ -0,0 +1,43 @@
+function playGame(playerSelection, computerSelection) {
+  if (playerSelection === "rock") {
+      if (computerSelection === "rock") {
+          return "It's a draw";
+      } else if (computerSelection === "paper") {
+          return "Computer wins!!!";
+      } else {
+          return "User wins!!!";
+      }
+
+  } else if (playerSelection === "paper") {
+      if (computerSelection === "rock") {
+          return "User wins!!!";
+      } else if (computerSelection === "paper") {
+          return "It's a draw!!!"
+      } else {
+          return "Computer wins!!!";
+      }
+
+  } else {
+      if (computerSelection === "rock") {
+          return "Computer Wins!!!";
+      } else if (computerSelection === "paper") {
+          return "User wins!!!";
+      } else {
+          return "Its a draw!!!"
+      }
+  }
+}
+
+function computerPlay() { 
+  let random = ["rock", "paper", "scissors"];
+  return random[Math.floor(Math.random() * 3)];
+}
+
+
+var userChoice = 'scissors';
+var computerSelection = computerPlay();
+var result = playGame(userChoice, computerSelection)
+
+console.log("Your choice", userChoice);
+console.log("Computer's choice", computerSelection);
+console.log("The winner is: ", result);
